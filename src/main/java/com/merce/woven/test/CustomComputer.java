e7a8fc55ce3cc924bd9b4a521b96c549639b7f55
jinjin.chang <jinjin.chang@inforefiner.com>
2022-10-08 11:02:11 +0800

    装饰者对象，2022-09-30 19:05(国亲节)

---
+++ src/main/java/com/merce/woven/test/CustomComputer.java
@@ -0,0 +1,43 @@
+//package com.merce.woven.create.test;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@ToString
+//public class CustomComputer {
+//    /**
+//     * cpu(必要)
+//     */
+//    private String cpu;
+//
+//    /**
+//     * 主板(必要)
+//     */
+//    private String board;
+//
+//    /**
+//     * 内存(必要)
+//     */
+//    private String ram;
+//
+////    private Builder builder;
+//
+//    public CustomComputer(){}
+//
+//    public CustomComputer(Builder builder){
+//        this.cpu = builder.cpu;
+//        this.board = builder.board;
+//        this.ram = builder.ram;
+//    }
+//
+//    public static Builder builder() {
+//        return new Builder();
+//    }
+////
+////    public CustomComputer build(){
+////        return new CustomComputer(builder);
+////    }
+//}
